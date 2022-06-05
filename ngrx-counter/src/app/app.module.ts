f02211e5f4c59293dc96899f8340e597c4439500
Joseph Ng <joseph_sieang@live.com>
2022-06-05 15:10:47 +0800

    Import store module in app module

--- ngrx-counter/src/app/app.module.ts
+++ ngrx-counter/src/app/app.module.ts
@@ -5,18 +5,18 @@ import { AppComponent } from './app.component';
 import { CounterComponent } from './counter/counter/counter.component';
 import { CounterDisplayComponent } from './counter/counter-display/counter-display.component';
 import { CounterButtonsComponent } from './counter/counter-buttons/counter-buttons.component';
+import { StoreModule } from '@ngrx/store';
+import { counterReducer } from './counter/state/counter.reducers';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CounterComponent,
-    CounterDisplayComponent,
-    CounterButtonsComponent
-  ],
+  declarations: [AppComponent, CounterComponent, CounterDisplayComponent, CounterButtonsComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot({
+      counter: counterReducer
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
