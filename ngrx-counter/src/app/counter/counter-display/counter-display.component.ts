710132d5a72a98244de835c65ea5e7bc8154e349
Joseph Ng <joseph_sieang@live.com>
2022-06-05 15:11:18 +0800

    Implement counter display components

--- ngrx-counter/src/app/counter/counter-display/counter-display.component.ts
+++ ngrx-counter/src/app/counter/counter-display/counter-display.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Counter } from '../state/counter.state';
 
 @Component({
   selector: 'app-counter-display',
@@ -6,10 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./counter-display.component.scss']
 })
 export class CounterDisplayComponent implements OnInit {
+  count: number = 0;
 
-  constructor() { }
+  constructor(private store: Store<{ counter: Counter }>) {}
 
   ngOnInit(): void {
+    this.store.select('counter').subscribe({ next: (val) => (this.count = val.counter) });
   }
-
 }
