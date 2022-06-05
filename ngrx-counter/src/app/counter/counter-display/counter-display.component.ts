5e380934f7dc03056faebb8b67354731951dd243
Joseph Ng <joseph_sieang@live.com>
2022-06-05 15:34:26 +0800

    Using async pipe for selecting observable state data

--- ngrx-counter/src/app/counter/counter-display/counter-display.component.ts
+++ ngrx-counter/src/app/counter/counter-display/counter-display.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { map, Observable, Subject, takeUntil } from 'rxjs';
 import { CounterState } from '../state/counter.state';
 
 @Component({
@@ -7,12 +8,24 @@ import { CounterState } from '../state/counter.state';
   templateUrl: './counter-display.component.html',
   styleUrls: ['./counter-display.component.scss']
 })
-export class CounterDisplayComponent implements OnInit {
+export class CounterDisplayComponent implements OnInit, OnDestroy {
   count: number = 0;
+
+  private unsubscription: Subject<void> = new Subject<void>();
 
   constructor(private store: Store<{ counter: CounterState }>) {}
 
-  ngOnInit(): void {
-    this.store.select('counter').subscribe({ next: (val) => (this.count = val.counter) });
+  ngOnInit(): void {}
+
+  ngOnDestroy(): void {
+    this.unsubscription.next();
+    this.unsubscription.complete();
+  }
+
+  getCounter(): Observable<number> {
+    return this.store.select('counter').pipe(
+      map((val) => val.counter),
+      takeUntil(this.unsubscription)
+    );
   }
 }
