b781fabad3c35eee124c9905555b836831a78328
Joseph Ng <joseph_sieang@live.com>
2022-06-19 17:22:04 +0800

    Remove unwanted api calls

--- ngrx-counter/src/app/posts/state/posts-effects.ts
+++ ngrx-counter/src/app/posts/state/posts-effects.ts
@@ -3,9 +3,11 @@ import { Router } from '@angular/router';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { Update } from '@ngrx/entity';
 import { RouterNavigationAction, ROUTER_NAVIGATION } from '@ngrx/router-store';
-import { filter, map, mergeMap, switchMap, tap } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { filter, map, mergeMap, of, switchMap, tap, withLatestFrom } from 'rxjs';
 import { Post } from 'src/app/models/post';
 import { PostsService } from 'src/app/services/posts.service';
+import { AppState } from 'src/app/store/app.state';
 import { RouterStateUrl } from 'src/app/store/router/custom-serializer';
 import {
   addPost,
@@ -17,20 +19,26 @@ import {
   updatePost,
   updatePostSuccess
 } from './posts.actions';
+import { getPosts } from './posts.selectors';
 
 @Injectable()
 export class PostsEffects {
-  constructor(private action$: Actions, private postsService: PostsService, private router: Router) {}
+  constructor(private action$: Actions, private postsService: PostsService, private router: Router, private store: Store<AppState>) {}
 
   loadPosts$ = createEffect(() => {
     return this.action$.pipe(
       ofType(loadPosts),
-      mergeMap((_action) => {
-        return this.postsService.getPosts().pipe(
-          map((data) => {
-            return loadPostsSuccess({ posts: data });
-          })
-        );
+      withLatestFrom(this.store.select(getPosts)),
+      mergeMap(([action, posts]) => {
+        if (posts?.length === 0 || posts?.length === 1) {
+          return this.postsService.getPosts().pipe(
+            map((data) => {
+              return loadPostsSuccess({ posts: data });
+            })
+          );
+        } else {
+          return of();
+        }
       })
     );
   });
@@ -102,14 +110,19 @@ export class PostsEffects {
       map((action) => {
         return action.payload.routerState.params['id'];
       }),
-      switchMap((id: string) =>
-        this.postsService.getPostById(id).pipe(
-          map((post) => {
-            const postWithId: Post = { ...post, id };
-            return loadPostsSuccess({ posts: [postWithId] });
-          })
-        )
-      )
+      withLatestFrom(this.store.select(getPosts)),
+      switchMap(([id, posts]: [string, Post[]]) => {
+        if (posts?.length > 0) {
+          return of();
+        } else {
+          return this.postsService.getPostById(id).pipe(
+            map((post) => {
+              const postWithId: Post = { ...post, id };
+              return loadPostsSuccess({ posts: [postWithId] });
+            })
+          );
+        }
+      })
     );
   });
 }
